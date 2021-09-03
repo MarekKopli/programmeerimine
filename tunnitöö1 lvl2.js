1d269ab54d77b657d46e462459b4e9c9b8875dc2
Marek Kopli <90028455+MarekKopli@users.noreply.github.com>
2021-09-03 11:57:43 +0300

    Update tunnitöö1 lvl2.js

--- tunnitöö1 lvl2.js
+++ tunnitöö1 lvl2.js
@@ -1,9 +1,9 @@
 rahakott = Math.random()*50
 tootehind=25
 if (rahakott>tootehind) 
-      console.log("ostan!") ;
+      console.log("ostan!"+" "+"Alles jääb"+" "+(rahakott-tootehind)) ;
 else
-     console.log("tõmban minema!");
+     console.log("tõmban minema!"+" "+"puudu jääb"+(tootehind-rahakott));
 console.log(rahakott-tootehind)
-VM4427:6 tõmban minema!
-VM4427:7 -22.525140125518885
+VM198:6 tõmban minema! puudu jääb12.27884632214964
+VM198:7 -12.27884632214964
