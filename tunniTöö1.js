b9a214435e4e13ebbf4cc8d9d37a58e5167213f0
Marek Kopli <90028455+MarekKopli@users.noreply.github.com>
2021-09-03 12:02:21 +0300

    asd

--- tunniTöö1.js
+++ tunniTöö1.js
@@ -3,7 +3,7 @@ tootehind=25
 if (rahakott>tootehind) 
       console.log("ostan!"+" "+"Alles jääb"+" "+(rahakott-tootehind)) ;
 else
-     console.log("tõmban minema!"+" "+"puudu jääb"+(tootehind-rahakott));
+     console.log("tõmban minema!"+" "+"puudu jääb"+" "+(tootehind-rahakott));
 console.log(rahakott-tootehind)
 VM198:6 tõmban minema! puudu jääb12.27884632214964
 VM198:7 -12.27884632214964
