415a6407f95f7a73757ab188d865cb2185ed682e
Marek Kopli <90028455+MarekKopli@users.noreply.github.com>
2021-09-10 10:12:45 +0300

    Update and rename märkmed to checkTemp.js

---
+++ checkTemp.js
@@ -0,0 +1,18 @@
+/* liiga külm 0-20
+okei 21-40
+liiga kuum
+*/
+function checkTemp() {
+  let temp=5 
+
+if(temp<20){
+console.log("liiga"+" "+"külm");
+
+}if(temp>40){
+console.log("liiga kuum");
+
+}else if(temp>20 && temp<=40)
+console.log("Väga hea")
+
+
+}
