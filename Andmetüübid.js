635166ae52a7999566074d013931483105f8fd92
Marek Kopli <90028455+MarekKopli@users.noreply.github.com>
2021-09-10 09:22:25 +0300

    Update Andmetüübid.js

--- Andmetüübid.js
+++ Andmetüübid.js
@@ -4,5 +4,25 @@ let töötaja={
   nimi: "Timo",
   palk: 10,
 }  
+// CTRL+K CTRL+C //
+
+Function summa(){
+  
+}
+// typeof (summa) ; Function
+
+// primitive types:
+// number, string, boolean, object, function, undefined, null
+
+// user defined types:
+// Math
 
 
+
+/* liiga külm 0-20
+okei 21-40
+liiga kuum
+*/
+function checkTemp() {
+   let temp = 5 
+}
