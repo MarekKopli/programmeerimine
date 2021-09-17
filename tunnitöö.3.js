9127d2a108e28f51dcaf4da487a20e62a59d3f68
Marek Kopli <90028455+MarekKopli@users.noreply.github.com>
2021-09-17 12:12:46 +0300

    Update tunnitöö.3.js

--- tunnitöö.3.js
+++ tunnitöö.3.js
@@ -3,6 +3,7 @@ let et = [,1,2,3,4,5,6,7,8,9,10,11,12];
 console.log(et[tahaTeada-1]); //-1 kui tahad, et 0 ei töötaks//
 
 //i võib vahetada välja j-ga
+//
 for ( let i=1; i<=100; i++) {
   console.log(i)
 }
