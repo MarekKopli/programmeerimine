07413fcb18ac592ccff13fd67223d00abfd98211
Marek Kopli <90028455+MarekKopli@users.noreply.github.com>
2021-09-17 10:33:56 +0300

    Create tunnitöö.3.js

---
+++ tunnitöö.3.js
@@ -0,0 +1,16 @@
+let tahaTeada = prompt("Mis kuu?");
+let et = [,1,2,3,4,5,6,7,8,9,10,11,12];
+console.log(et[tahaTeada-1]); //-1 kui tahad, et 0 ei töötaks//
+
+//i võib vahetada välja j-ga
+for ( let i=1; i<=100; i++) {
+  console.log(i)
+}
+ 
+let tööta = true;
+while(tööta) {
+console.log(1);
+  if (new Date().getMinutes() >=27) {
+    tööta= false;
+}
+}
