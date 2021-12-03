9c9a157c78c621a4ae8fa175402abc5b6ae46270
Marek Kopli <90028455+MarekKopli@users.noreply.github.com>
2021-12-03 10:57:47 +0200

    Update igast ülesanded.js

--- igast ülesanded.js
+++ igast ülesanded.js
@@ -1,3 +1,4 @@
+//1.
 const array = [2,7,6,12,44,32,4,8,12,17];
 let sum = 0;
 
@@ -7,7 +8,7 @@ for (let i = 0; i < array.length; i++) {
 console.log(sum);
 // liidab kõik kokku
 ---------------------------------
-//stringi kokku liitmine
+//2. stringi kokku liitmine
 let str= "2 7 6 12 44 32 4 8 12 17";
 str = str.replace(/ /g, ",");
 var b = str.split(',').map(function(item) {
@@ -20,10 +21,21 @@ for (let i = 0; i < array.length; i++) {
 }
 console.log(sum);
 ----------------------------------------------
+//3. aja kokku liitmine
 var aeg = 7582;
 var tunnid = Math.floor(aeg / 60 / 60);
 var minutid = Math.floor(aeg / 60) - (tunnid * 60);
 var sekundid = aeg % 60;
 
 var kokkupandud = tunnid + ':' + minutid + ':' + sekundid;
+console.log(kokkupandud)
+-----------------------------------------
+//5.
+var aeg = 79134;
+var päevad =  Math.floor(aeg / 60 / 60/24);
+var tunnid = Math.floor(aeg / 60 / 60);
+var minutid = Math.floor(aeg / 60) - (tunnid * 60);
+var sekundid = aeg % 60;
+
+var kokkupandud =päevad+':'+ tunnid + ':' + minutid + ':' + sekundid;
 console.log(kokkupandud)
